fef67d318c5f2b4c0920e9d3482d9d7e195efbaa
loda <loda.namnh@gmail.com>
2019-08-18 10:49:29 +0700

    change default popup address

--- popup.js
+++ popup.js
@@ -3,13 +3,12 @@ chrome.tabs.executeScript(
     code: "window.getSelection().toString();"
   },
   function(selection) {
-    if (!isBlank(selection[0])) {
+    var url = "https://www.oxfordlearnersdictionaries.com/us/definition/english/";
+    if (selection && !isBlank(selection[0])) {
       var query = encodeURIComponent(selection[0].trim().toLowerCase());
-      var url = "https://www.oxfordlearnersdictionaries.com/definition/english/" + query;
-      document.querySelector("iframe").src = url;
-    } else {
-      window.close();
+      url = url + query;
     }
+    document.querySelector("iframe").src = url;
   }
 );
 
